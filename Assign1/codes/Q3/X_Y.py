715ff92e46c2566e82198f3ade6593788bea1e95
Bhanu-das <110126700+Bhanu-das@users.noreply.github.com>
2022-08-10 11:23:57 +0530

    Create X_Y.py

---
+++ Assign1/codes/Q3/X_Y.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+
+
+
+x=np.array([1.0,2.0,3.0,4.0,2.0,1.0])
+k = 20
+y = np.loadtxt('xnyn.dat',dtype='double')
+
+
+#subplots
+plt.subplot(2, 1, 1)
+plt.stem(range(0,6),x,use_line_collection=True)
+plt.title('Digital Filter Input-Output')
+plt.ylabel('$x(n)$')
+plt.grid()# minor
+
+
+plt.subplot(2, 1, 2)
+plt.stem(range(0,k),y,use_line_collection=True)
+plt.xlabel('$n$')
+plt.ylabel('$y(n)$')
+plt.grid()# minor
+
+plt.savefig("dig.png")
